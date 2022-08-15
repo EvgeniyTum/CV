355dd9efa8fe40e7d5c68e8930b6e683f3c839e8
EvgenyTum <gotic@list.ru>
2022-08-16 00:21:54 +0300

    add gill_form.js & change index.html(add button fill-form)

---
+++ fill_form.js
@@ -0,0 +1,9 @@
+function fillForm () {
+  document.getElementById('name').value = prompt('Введите ваше имя', '');
+  document.getElementById('email').value = prompt('Введите ваш email', '');
+  document.getElementById('text').value = prompt('Введите ваше сообщение', '');
+};
+
+
+
+
